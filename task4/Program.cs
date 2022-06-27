d596d649cacb380276fec388aaf8eec89e0c6116
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-27 12:18:52 +0300

    Выполнили 4 задачу

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task4
+Console.WriteLine("Введите первое число для нахлждения максимального заначения");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для нахлждения максимального заначения");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число для нахлждения максимального заначения");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max)  max = numberB;
+if (numberC > max) max = numberC;
+Console.WriteLine($"Максимальное число = {max}");
