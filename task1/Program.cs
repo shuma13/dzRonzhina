9e8ac031bd5f6cd8eea7b2eda0e98105fe7487ce
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-27 11:22:31 +0300

    выполнили 6 задачу

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task6
+Console.WriteLine("Введите число для проверки на четность");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number % 2;
+bool result = temp ==0;
+if (result)
+{
+    Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} является нечетным");
+}
