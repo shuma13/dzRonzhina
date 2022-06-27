d94c5eea52f2def4b9fc3ff8124d6ddad377d3a8
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-27 11:46:03 +0300

    Выполнили задачу 2

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task2
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше числа {numberB}");
+}
+else if (numberB > numberA)
+{
+    Console.WriteLine($"Число {numberB} больше числа {numberA}");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} равно числу {numberB}");
+}
