0c50e86fe7d2fa8e6bffe06d51a20e11c9b6f00e
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-30 00:56:32 +0300

    Выполнили задачу 2 семинара на кратность

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача со 2 семинара
+Console.WriteLine("Введите число для проверки кратности 7 и 23");
+int number = Convert.ToInt32(Console.ReadLine());
+int c = number % 23;
+int d = number % 7;
+if (c ==0 && d == 0)
+{
+    System.Console.WriteLine("Число одновремено кратно 7 и 23");
+}
+else 
+{
+    System.Console.WriteLine("Нет - Число одновремено не кратно 7 и 23");
+}
