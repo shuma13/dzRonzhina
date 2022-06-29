1bf677dcf5b7609cbf08265621795cad94cb0443
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-29 23:47:13 +0300

    Выполнили задачу 10 2 семинар

---
+++ task5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int c = (number / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {number}");
+Console.WriteLine(c);
+
