bc95dac812a54a065a37e9760a5453cf7caa7ace
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-06-30 00:01:16 +0300

    Выполнили задачу 15 2семинар

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15 второй семинар
+Console.WriteLine("Введите цифру от 1 до 7, соответствующую дню недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1: Console.WriteLine("Нет - день не является выходным"); break;
+    case 2: Console.WriteLine("Нет - день не является выходным"); break;
+    case 3: Console.WriteLine("Нет - день не является выходным"); break;
+    case 4: Console.WriteLine("Нет - день не является выходным"); break;
+    case 5: Console.WriteLine("Нет - день не является выходным"); break;
+    case 6: Console.WriteLine("Да - день является выходным"); break;
+    case 7: Console.WriteLine("Да - день является выходным"); break;
+}
