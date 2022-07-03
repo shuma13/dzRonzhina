d57e8dd2450e4db1fe18bd7344e16a80c63cff68
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-04 00:08:54 +0300

    Выполнили задачу 23 3 семинар

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23 3 семинар
+Console.WriteLine("Введите число N");
+int i;
+int N = Convert.ToInt32(Console.ReadLine());
+double cube;
+for(i=1; i<=N; i++)
+{
+    cube = Math.Pow(i,3);
+    System.Console.Write(cube);
+    if (i!=N)
+    {
+        System.Console.Write(",");
+    }
+}
