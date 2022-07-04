e2e50180d4ed73e77f3752035ea46d84e4afdc48
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-04 10:30:12 +0300

    выполнили 19 задачу 3 семинар

---
+++ task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19 3 семинар
+Console.WriteLine("Введите number для проверки является ли оно полиндромом");
+string some_number = Console.ReadLine();
+char[]array = some_number.ToCharArray();
+int count = array.Length;
+int i = 0;
+while (i<count/2)
+{
+    if(array[i] == array[count-1])
+    {
+        count= count-1;
+        i++;
+        System.Console.WriteLine("Заданное число является полиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Заданное число не является полиндромом");
+        break;
+    }
+}
