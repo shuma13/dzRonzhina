d586bf9195dc97208609c49d8469e19f4edd1fda
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-04 16:50:30 +0300

    Выполнили задачу 21 3 семинар

---
+++ task9/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 21 3 семинар
+Console.WriteLine("Введите координату первой точки по оси X");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки по оси Y");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки по оси Z");
+double Az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси X");
+double Bx = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси Y");
+double By = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси Z");
+double Bz = Convert.ToDouble(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((Ax-Bx),2)+Math.Pow((Ay-By),2)+Math.Pow((Az-Bz),2));
+System.Console.WriteLine(distance);
