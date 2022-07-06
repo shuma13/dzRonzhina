6ff6e5b10f7211c3b99bcc290d24f39dcb983898
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-06 16:11:22 +0300

    Выполнили задачу 25 4 семинар

---
+++ task25/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+int GetPowFunc(int numA, int numB)
+{
+    int power=1;
+    for (int i = 0; i < numB; i++)
+    {
+        power *= numA;
+    }
+    return power;
+}
+Console.WriteLine("Введите число A для возведения в степень");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B - степень для числа A");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int res =GetPowFunc(numberA, numberB);
+System.Console.WriteLine($"{numberA} в степени {numberB} равно {res}");
