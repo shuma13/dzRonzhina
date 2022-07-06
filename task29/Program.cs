ce7132b684b0ba3c1ae215a65c42345bf8ae1ac3
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-07 00:24:00 +0300

    Выполнили 29 задачу 4 семинар

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+int []array = new int[8];
+void FillArray(int [] coll)
+{
+    int length = coll.Length;
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.WriteLine("введите значение для массива");
+        int num = Convert.ToInt32(Console.ReadLine());
+        coll[i] = num;
+    }
+}
+FillArray(array);
+foreach (var item in array)
+{
+   Console.Write(item +", "); 
+}
