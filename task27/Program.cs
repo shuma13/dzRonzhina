48eafb00c9dadc823cf6f80be584a5068844ebc9
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-06 23:54:55 +0300

    Выполнили 27 задачу 4 семинар

---
+++ task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+int GetSumofNumbers(int num)
+{
+    int temp=0;
+    while (num > 10)
+    { 
+        int c = num % 10;
+        temp = temp + c;
+        num = num / 10;
+    }
+    temp = temp + num;
+    return temp;
+}
+System.Console.WriteLine("Введите число для нахождения суммы всех его чисел");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = GetSumofNumbers(number);
+System.Console.WriteLine(res);
