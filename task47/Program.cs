e80b3ad179619fd2dad5062592855bc5a61454b8
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-16 00:15:10 +0300

    Выполнили 47 задачу 7 семинар

---
+++ task47/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+*/
+double[,] GenerateArray(int rowNumber, int colNumber)
+{
+    double[,] result = new double[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(1000,10000)/ 100.0;
+        }
+    }
+    return result;
+}
+void printGreen(int number)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"{number}\t");
+    Console.ResetColor();
+}
+void print2DArray(double[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (var i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printGreen(i);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printGreen(i);
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j]);
+            if (j < arrayToPrint.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите размерность массива - количество сторок :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размерность массива - количество столбцов :");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = GenerateArray(m, n);
+print2DArray(array);
+
+
