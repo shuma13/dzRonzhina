49433da34e8c147788d6235d55530c520305d28e
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-07-21 16:09:53 +0300

    Ввыполнили 54 задачу 8 семинар

---
+++ task54/Program.cs
@@ -0,0 +1,84 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+//элементы каждой строки двумерного массива.
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+В итоге получается вот такой массив:
+
+1 2 4 7
+
+2 3 5 9
+
+2 4 4 8*/
+
+int[,] GenerateArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(- deviation, deviation+1);
+        }
+    }
+    return result;
+}
+
+int[,] GetRegulateArray(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] > matrix[i, k])
+                {
+                temp = matrix[i, j];
+                matrix[i, j] = matrix[i, k];
+                matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+void PrintGreen(int number)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"{number}\t");
+    Console.ResetColor();
+}
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (var i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        PrintGreen(i);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        PrintGreen(i);
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j]);
+            if (j < arrayToPrint.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+int[,] masiv = GenerateArray(4, 4,10);
+Print2DArray(masiv);
+Console.WriteLine();
+int[,] result = GetRegulateArray(masiv);
+Print2DArray(result);
